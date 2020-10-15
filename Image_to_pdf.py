7772ebfdb7b3ad53995fe39344db0fc659794bc7
Sanjana <46816463+Born-to-shine@users.noreply.github.com>
2020-10-15 12:54:08 +0530

    Add files via upload

---
+++ Image_to_pdf.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 12:38:37 2020
+
+@author: user
+"""
+
+import os
+from PIL import Image
+def image_pdf(image_path):
+  file_name = str(image_path.split("\\")[-1])
+  new_file = os.path.join(image_path.replace(file_name.split(".")[-1], "pdf"))
+  image_file = Image.open(image_path)
+  parsed = image_file.convert("RGB")
+  return parsed.save(new_file)
+if __name__ == "__main__":
+  image_path = 'cup_coffee_books.jpg'
+  pdf = image_pdf(image_path)
+  print(image_path)
